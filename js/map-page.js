6308e6aa97c4f37d56ed15a45d9acf3722cfbd87
kitsandkats <katherine.mcconachie@gmail.com>
2016-11-21 14:07:08 -0500

    adding new map, small tweaks

--- js/map-page.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $('#map-container').prepend("<iframe frameborder=0 style='width:100%;height:100%' src='//www.zeemaps.com/pub?group=2155997&list=1&simpleadd=1&x=-57.886962&y=-11.383109&z=13'> </iframe>");    
-});
